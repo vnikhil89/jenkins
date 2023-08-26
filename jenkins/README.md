17b807fececc9dd84604f4861c28a8955017d42a
Nikhil Verma <n.nikhilverma89@gmail.com>
2023-08-26 13:09:25 +0530

    Uploading Readme

---
+++ jenkins/README.md
@@ -0,0 +1,1 @@
+Jenkins Integration with OCI
---
+++ jenkins/jenkins.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+sudo dnf install curl 
+sudo dnf install -y java-11-openjdk
+sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo sed -i --follow-symlinks 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/jenkins.repo
+sudo dnf install -y jenkins
+sudo systemctl start jenkins && systemctl enable jenkins
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
